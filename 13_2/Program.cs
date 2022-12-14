d824c96779825c3957bd4a2e80ac5e8dec56ebf6
Арсений Мазуренко <arseniy.mazurenko.03@gmail.com>
2022-12-14 16:32:53 +0300

    Добавьте файлы проекта.

---
+++ 13_2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Thread thread = new Thread(Print);
+        thread.Start();
+
+        for (int i = 0; i < 5; i++)
+        {
+            Console.WriteLine($"Главный поток: {i}");
+            Thread.Sleep(300);
+        }
+
+        void Print()
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine($"Второй поток: {i}");
+                Thread.Sleep(400);
+            }          
+        }
+    }
+}
